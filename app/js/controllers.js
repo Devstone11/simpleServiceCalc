d99299c507ebb16be7954520721291e989fe4d28
Devin Stone <devstone11@gmail.com>
2016-08-18 09:36:06 -0600

    complete hypotenuse exercise

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,5 @@
-trig.controller("TrigController", function(){
-
-  
-
+trig.controller("TrigController", function($scope, CalculatorFactory){
+  $scope.calculate = function() {
+    $scope.hypotenuse = CalculatorFactory.calculate($scope.sideA, $scope.sideB)
+  }
 });
--- app/js/services.js
+++ app/js/services.js
@@ -1,7 +1,7 @@
 trig.factory("CalculatorFactory", function(){
-
-
-
-
-
+  return {
+    calculate: function(sideA, sideB) {
+      return Math.hypot(sideA, sideB);
+    }
+  }
 })
